ef8b8b9c1ecb3ac89ac3efb1bbe3093f57afb663
y2mtawooly <tawooly.chung@y2m.jp>
2023-02-10 16:08:07 +0900

    Init

---
+++ TestBoard/TestBoard/Data/TestBoardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestBoard.Models;
+
+namespace TestBoard.Data
+{
+    public class TestBoardContext : DbContext
+    {
+        public TestBoardContext (DbContextOptions<TestBoardContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestBoard.Models.BoardDB> BoardDB { get; set; } = default!;
+    }
+}
---
+++ TestBoard/TestBoard/Models/BoardDB.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TestBoard.Models
+{
+
+
+    public class BoardDB
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+        
+        public string? Text { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
+
+/* データモデルクラスのポイント
+ * 1. クラス名とテーブル名を一致させる
+ * 2. 各プロパティとテーブルの列名と一致させる
+ * 3. dプロパティがテーブルの主キーとなる */
