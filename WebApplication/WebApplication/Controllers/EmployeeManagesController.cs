0330d37a58be629b53fee73de0678723350ffb93
y2mtawooly <tawooly.chung@y2m.jp>
2023-02-16 14:18:26 +0900

    DB連動 - CRUD画面出力

---
+++ WebApplication/WebApplication/Controllers/EmployeeManagesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Data;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class EmployeeManagesController : Controller
+    {
+        private WebApplicationContext db = new WebApplicationContext();
+
+        // GET: EmployeeManages
+        public ActionResult Index()
+        {
+            return View(db.EmployeeManages.ToList());
+        }
+
+        // GET: EmployeeManages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeManage employeeManage = db.EmployeeManages.Find(id);
+            if (employeeManage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeManage);
+        }
+
+        // GET: EmployeeManages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeManages/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NAME,AGE,GENDER,PHONE,ADRESS")] EmployeeManage employeeManage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmployeeManages.Add(employeeManage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employeeManage);
+        }
+
+        // GET: EmployeeManages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeManage employeeManage = db.EmployeeManages.Find(id);
+            if (employeeManage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeManage);
+        }
+
+        // POST: EmployeeManages/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NAME,AGE,GENDER,PHONE,ADRESS")] EmployeeManage employeeManage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employeeManage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employeeManage);
+        }
+
+        // GET: EmployeeManages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeManage employeeManage = db.EmployeeManages.Find(id);
+            if (employeeManage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeManage);
+        }
+
+        // POST: EmployeeManages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EmployeeManage employeeManage = db.EmployeeManages.Find(id);
+            db.EmployeeManages.Remove(employeeManage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Migrations/202302160443283_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.EmployeeManages",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        NAME = c.String(),
+                        AGE = c.Int(nullable: false),
+                        GENDER = c.String(),
+                        PHONE = c.String(),
+                        ADRESS = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.EmployeeManages");
+        }
+    }
+}
