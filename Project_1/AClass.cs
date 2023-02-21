96ea128f748e779ea344e59a1ed4242856bce12e
y2mtawooly <tawooly.chung@y2m.jp>
2023-02-21 17:36:05 +0900

    Project_1 「変数とクラス」
    ①변수 선언, 초기화, 변수 값 넣기
    ②클래스 만들기, 클래스 객체 만들기, 클래스 메소드 호출하기
    ③만들어 놓은 클래스 사용 하는 방법

---
+++ Project_1/AClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    //클래스 만들기
+    public class AClass
+    {
+        public void aMethod(int b)
+        {
+            int a = 3;
+            Console.WriteLine(a - b);
+        }
+
+        public void bMethod(string a, string b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void cMethod(int a, string b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+    }
+}
---
+++ Project_1/DClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    public class DClass
+    {
+        public void d()
+        {
+            var cclass = new CClass();
+            cclass.c("HELLO");
+        }
+    }
+}
