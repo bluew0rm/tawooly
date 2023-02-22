c09c0ab9e436c13618eeb766dde18b8bc09b0872
y2mtawooly <tawooly.chung@y2m.jp>
2023-02-22 13:49:05 +0900

    Project_4 [ArrayList]
    ①배열 선언, 초기화, 배열 값 넣기
    ②사용 방법, 취득, 출력

---
+++ Project_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+var arrayList1 = new ArrayList();
+
+arrayList1.Add(1);
+arrayList1.Add("hello");
+
+
+
+var list = new List<String>();
+
+list.Add("1→x [1]→o");
+list.Add("hello");
+
+
+
+var arrayList2 = new ArrayList() {2, "hi", 4, "ABcd" };
---
+++ TestBoard/SamplePage/Models/Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SamplePage.Models
+{
+    public class Board
+    {
+        [Key]
+        public int ID { get; set; }
+        public string TITLE { get; set; }
+        public string KATEGORIES { get; set; }
+        public string WRITER { get; set; }
+    }
+}
