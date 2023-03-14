14fa6ddd279b79dcb745176d8d92b913a0227136
y2mtawooly <tawooly.chung@y2m.jp>
2023-03-14 19:55:17 +0900

    Project_Board [ビジネスロジックの分離(SELECT ALL)]

--- Project_Board/Controllers/BoardController.cs
+++ Project_Board/Controllers/BoardController.cs
@@ -1,7 +1,9 @@
 ﻿using Project_Board.Models;
+using Project_Board.Service.Adepter;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Web.Mvc;
 
@@ -9,202 +11,57 @@ namespace Project_Board.Controllers
 {
     public class BoardController : Controller
     {
-        protected static string connstring
-            = ConfigurationManager.ConnectionStrings["MemberConnectionString"].ConnectionString;
-
-        List<Boards> boardsModel = new List<Boards>();
-
-        SqlConnection conn = new SqlConnection(connstring);
+        
+        CallData boardAdepter = new CallData();
+        DataTable itemTable = new DataTable();
 
         public ActionResult Index()
         {
-            try
-            {
-                conn.Open();
-                string queryStr = "SELECT * FROM PostBoard";
+            boardAdepter.Index(itemTable);
 
-                SqlCommand cmd = new SqlCommand(queryStr, conn);
-
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        boardsModel.Add(new Boards(int.Parse(reader[0].ToString()), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString()));
-                    }
-                    return View(boardsModel);
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewData["DB接続不可"] = "接続不可";
-            }
-            return View();
+            return View(itemTable);
         }
 
         public ActionResult Create()
         {
-            try
-            {
-                conn.Open();
-                using (var connection = conn.CreateCommand())
-                {
-                    string queryStrInsert = "INSERT INTO PostBoard VALUES(@param1, @param2, @param3, @param4)";
-                    using (SqlCommand cmdIn = new SqlCommand(queryStrInsert, conn))
-                    {
-                        if (ModelState.IsValid)
-                        {
-                            var title = Request.Form["_title"];
-                            var text = Request.Form["_text"];
-                            var writer = Request.Form["_writer"];
-                            var date = Request.Form["_date"];
-
-                            cmdIn.Parameters.Add(new SqlParameter("@param1", title));
-                            cmdIn.Parameters.Add(new SqlParameter("@param2", text));
-                            cmdIn.Parameters.Add(new SqlParameter("@param3", writer));
-                            cmdIn.Parameters.Add(new SqlParameter("@param4", date));
-
-                            var excution = cmdIn.ExecuteNonQuery();
-                        }
-                        return Redirect("Index");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewData["DB接続不可"] = "接続不可";
-            }
             return View();
         }
-        
+
         public ActionResult AllDelete()
         {
-            try
-            {
-                conn.Open();
-                using (var connection = conn.CreateCommand())
-                {
-                    string queryStrDELETE = "DELETE FROM PostBoard";
-                    using (SqlCommand cmdIn = new SqlCommand(queryStrDELETE, conn))
-                    {
-                        if (ModelState.IsValid)
-                        {
-                            var excution = cmdIn.ExecuteNonQuery();
-                        }
-                        return Redirect("Index");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewData["DB接続不可"] = "接続不可";
-            }
-            return Redirect("Index");
+            return View();
         }
 
         [HttpPost]
         public ActionResult SingleDelete()
         {
-            try
-            {
-                conn.Open();
-                using (var connection = conn.CreateCommand())
-                {
-                    string queryStrDELETE = "DELETE FROM PostBoard WHERE [Id] = @param1;";
-                    using (SqlCommand cmdIn = new SqlCommand(queryStrDELETE, conn))
-                    {
-                        if (ModelState.IsValid)
-                        {
-                            var ID = Request.Form["_id"];
-
-                            cmdIn.Parameters.Add(new SqlParameter("@param1", ID));
-
-                            var excution = cmdIn.ExecuteNonQuery();
-                        }
-                        return Redirect("Index");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewData["DB接続不可"] = "接続不可";
-            }
-            return Redirect("Index");
+            return View();
         }
 
         [HttpPost]
         public ActionResult Detail()
         {
-            try
-            {
-                conn.Open();
-                using (var connection = conn.CreateCommand())
-                {
-                    string queryStrDetail = "SELECT * FROM PostBoard WHERE [Id] = @param1";
-                    using (SqlCommand cmdDetail = new SqlCommand(queryStrDetail, conn))
-                    {
-                        if (ModelState.IsValid)
-                        {
-                            var id = Request.Form["_id"];
-
-                            cmdDetail.Parameters.Add(new SqlParameter("@param1", id));
-
-                            var excution = cmdDetail.ExecuteNonQuery();
-
-                            using (SqlDataReader reader = cmdDetail.ExecuteReader())
-                            {
-                                while (reader.Read())
-                                {
-                                    boardsModel.Add(new Boards(int.Parse(reader[0].ToString()), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString()));
-                                }
-                                return View(boardsModel);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewData["DB接続不可"] = "接続不可";
-            }
             return View();
         }
 
         [HttpPost]
         public ActionResult Update()
         {
-            try
-            {
-                conn.Open();
-                using (var connection = conn.CreateCommand())
-                {
-                    string queryStrDetail = "UPDATE PostBoard SET [Title] = @param2, [Writer] = @param3, [Update] = @param4, [Text] = @param5 WHERE [Id] = @param1";
-                    using (SqlCommand cmdDetail = new SqlCommand(queryStrDetail, conn))
-                    {
-                        if (ModelState.IsValid)
-                        {
-                            var id = Request.Form["_id"];
-                            var title = Request.Form["_title"];
-                            var Writer = Request.Form["_Writer"];
-                            var Update = Request.Form["_Update"];
-                            var Text = Request.Form["_Text"];
-
-                            cmdDetail.Parameters.Add(new SqlParameter("@param1", id));
-                            cmdDetail.Parameters.Add(new SqlParameter("@param2", title));
-                            cmdDetail.Parameters.Add(new SqlParameter("@param3", Writer));
-                            cmdDetail.Parameters.Add(new SqlParameter("@param4", Update));
-                            cmdDetail.Parameters.Add(new SqlParameter("@param5", Text));
-
-                            var excution = cmdDetail.ExecuteNonQuery();
-                            return Redirect("Index");
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewData["DB接続不可"] = "接続不可";
-            }
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Pageing()
+        {
+            boardAdepter.Paging(itemTable);
+
+            return View(itemTable);
         }
     }
 }
---
+++ Project_Board/Service/Adepter/CallData.cs
@@ -0,0 +1,225 @@
+﻿using Project_Board.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_Board.Service.Adepter
+{
+    public class CallData
+    {
+        //데이터
+        protected static string connstring
+            = ConfigurationManager.ConnectionStrings["MemberConnectionString"].ConnectionString;
+
+
+        SqlConnection conn = new SqlConnection(connstring);
+        public void Index(DataTable itemTable)
+        {
+            try
+            {
+                conn.Open();
+                string queryStr = "SELECT * FROM PostBoard";
+
+                SqlCommand cmd = new SqlCommand(queryStr, conn);
+
+                var adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(itemTable);
+                /*using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        boardsModel.Add(new Boards(int.Parse(reader[0].ToString()), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString()));
+                    }
+                }*/
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public void Paging(DataTable itemTable)
+        {
+            try
+            {
+                conn.Open();
+                string queryStr = "SELECT * FROM PostBoard";
+
+                SqlCommand cmd = new SqlCommand(queryStr, conn);
+
+                var adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(itemTable);
+
+                DataRow totalPage = itemTable.Rows[0]; //現在のレコード数
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
+/*
+
+
+        public void AllDelete()
+        {
+            DataTable itemTable = new DataTable();
+            try
+            {
+                conn.Open();
+                using (var connection = conn.CreateCommand())
+                {
+                    connection.CommandText = "DELETE FROM PostBoard";
+
+                    var adapter = new SqlDataAdapter(connection);
+                    adapter.Fill(itemTable);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        [HttpPost]
+        public void SingleDelete()
+        {
+            DataTable itemTable = new DataTable();
+            try
+            {
+                conn.Open();
+                using (var connection = conn.CreateCommand())
+                {
+                    connection.CommandText = "DELETE FROM PostBoard WHERE [Id] = @param1;";
+
+                    var ID = Request.Form["_id"];
+
+                    connection.Parameters.AddWithValue("@param1", ID);
+
+                    var adapter = new SqlDataAdapter(connection);
+                    adapter.Fill(itemTable);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        [HttpPost]
+        public void Detail()
+        {
+            try
+            {
+                conn.Open();
+                using (var connection = conn.CreateCommand())
+                {
+                    connection.CommandText = "SELECT * FROM PostBoard WHERE [Id] = @param1";
+
+                    var id = this.Request.Form["_id"];
+
+                    connection.Parameters.AddWithValue("@param1", id);
+
+                    //var excution = cmdDetail.ExecuteNonQuery();
+
+                    using (SqlDataReader reader = connection.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            boardsModel.Add(new Boards(int.Parse(reader[0].ToString()), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString()));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        [HttpPost]
+        public void Update()
+        {
+            DataTable itemTable = new DataTable();
+            try
+            {
+                conn.Open();
+                using (var connection = conn.CreateCommand())
+                {
+
+                    //テキストボックスのキーワード
+                    var id = Request.Form["_id"];
+                    var title = Request.Form["_title"];
+                    var Writer = Request.Form["_Writer"];
+                    var Update = Request.Form["_Update"];
+                    var Text = Request.Form["_Text"];
+
+                    // SQLの設定
+                    connection.CommandText = "UPDATE PostBoard SET [Title] = @param2, [Writer] = @param3, [Update] = @param4, [Text] = @param5 WHERE [Id] = @param1"; ;
+
+                    // SQLの実行
+                    connection.Parameters.AddWithValue("@param1", id);
+                    connection.Parameters.AddWithValue("@param2", title);
+                    connection.Parameters.AddWithValue("@param3", Writer);
+                    connection.Parameters.AddWithValue("@param4", Update);
+                    connection.Parameters.AddWithValue("@param5", Text);
+
+                    var adapter = new SqlDataAdapter(connection);
+                    adapter.Fill(itemTable);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        [HttpPost]
+        public void Search()
+        {
+            DataTable itemTable = new DataTable();
+            // 接続文字列の取得
+            var connectionString = ConfigurationManager.ConnectionStrings["MemberConnectionString"].ConnectionString;
+            using (var connection = new SqlConnection(connectionString))
+
+            using (var command = connection.CreateCommand())
+            {
+                try
+                {
+                    // データベースの接続開始
+                    connection.Open();
+                    //テキストボックスのキーワード
+                    string keywordText = Request.Form["_searchTitle"];
+
+
+                    // SQLの設定
+                    command.CommandText = @"SELECT * FROM PostBoard WHERE Title LIKE N'%' + @Keyword + N'%'";
+                    //command.CommandText = @"SELECT * FROM PostBoard WHERE Title LIKE N'%@Keyword%'";
+
+                    command.Parameters.AddWithValue("@Keyword", keywordText);      // SQLの実行
+                    var adapter = new SqlDataAdapter(command);
+                    adapter.Fill(itemTable);
+
+                }
+                catch (Exception exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    // データベースの接続終了
+                    connection.Close();
+                }
+            }
+        }
+    }
+}*/
